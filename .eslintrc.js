9d0179a1716882d1b86df3dadffd7002eb3c7e1c
Clement Caylux <cca@activeviam.com>
2021-04-04 17:22:53 +0200

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,15 +12,22 @@ module.exports = {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
         },
     },
+    env: {
+        browser: true, // let eslint know we are running a browser based app and avoid document undefined errors
+    },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+        'airbnb',
+        'airbnb/hooks',
         // "prettier", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array. This has to be the last config of the array
     ],
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        'no-use-before-define': 'off', // turning off because of  Weird eslint error appearing when we import React...
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // allow both .js and .jsx for React
     },
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import { React } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <header className="App-header">
@@ -16,6 +16,6 @@ function App() {
             </header>
         </div>
     );
-}
+};
 
 export default App;
